1cc54195d1277a01b91555757ddd8f3131ba7784
Darshana Mahesh <139673873+Maheshwickramage@users.noreply.github.com>
2024-05-12 23:39:05 +0530

    updated-provider

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -5,8 +5,12 @@ terraform {
       version = "5.49.0"
     }
   }
-}
-
+}   
+backend "s3" {
+    bucket = "newbucket4278"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 provider "aws" {
   region="us-east-1"
 }
